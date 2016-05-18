58ca5f3039d93a5dd7ae2804f01fc75f99c106e9
Uki19 <uroszivaljevic19@gmail.com>
2016-05-18 03:40:36 +0200

    updated category model

--- models/category.js
+++ models/category.js
@@ -4,9 +4,9 @@
 'use strict';
 module.exports = function(sequelize, DataTypes){
     var Category = sequelize.define('Category',{
-        name: DataTypes.STRING,
-        description: DataTypes.TEXT
+        name: DataTypes.STRING
     },{
+        timestamps: false,
         classMethods: {
             associate: function(models){
                 Category.belongsToMany(models.Doctor, {through: 'DoctorCategory', foreignKey:'categoryId'});
